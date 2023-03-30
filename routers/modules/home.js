475bcab341628cfdfd91b74460643cd814a48ffb
klu0926 <lukuoyu@gmail.com>
2023-03-31 00:11:14 +0800

    feat: require valid-url module to check url valid

--- routers/modules/home.js
+++ routers/modules/home.js
@@ -1,9 +1,33 @@
 // home
 const Router = require('express').Router()
+const validUrl = require('valid-url')
 
 
-// home
+
+// GET
 Router.get('/', (req, res) => {
+  res.render('index')
+})
+
+// POST
+Router.post('/', (req, res) => {
+  const url = req.body.url
+  let errorMessage = ""
+
+  // check input
+  if (url.trim() === "") {
+    errorMessage = "Please enter your url"
+    return res.render('index', { errorMessage })
+  }
+
+  // check valid url
+  if (!(validUrl.isWebUri(url))) {
+    errorMessage = 'Please enter a valid URL like : "http://www.google.com" '
+    return res.render('index', { errorMessage })
+  }
+
+  console.log(url)
+
   res.render('index')
 })
 
